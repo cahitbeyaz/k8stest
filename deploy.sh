63edd1a9cfd0c56486d77e4d0f78619f580c627f
cahit.beyaz <cahit.beyaz@exagate.com>
2020-05-04 11:41:40 +0300

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t cahitbeyaz/multi-client -t cahitbeyaz/mult-client:latest -t cahitbeyaz/mult-client:$GIT_SHA -f ./client/Dockerfile .client
-docker build -t cahitbeyaz/multi-server -t cahitbeyaz/mult-server:latest -t cahitbeyaz/mult-server:$GIT_SHA -f ./server/Dockerfile .server
-docker build -t cahitbeyaz/multi-worker -t cahitbeyaz/mult-worker:latest -t cahitbeyaz/mult-worker:$GIT_SHA -f ./worker/Dockerfile .worker
+docker build -t cahitbeyaz/mult-client:latest -t cahitbeyaz/mult-client:$GIT_SHA -f ./client/Dockerfile .client
+docker build -t cahitbeyaz/mult-server:latest -t cahitbeyaz/mult-server:$GIT_SHA -f ./server/Dockerfile .server
+docker build -t cahitbeyaz/mult-worker:latest -t cahitbeyaz/mult-worker:$GIT_SHA -f ./worker/Dockerfile .worker
 docker push cahitbeyaz/multi-client:latest
 docker push cahitbeyaz/multi-server:latest
 docker push cahitbeyaz/multi-worker:latest
@@ -8,6 +8,6 @@ docker push cahitbeyaz/multi-client:$GIT_SHA
 docker push cahitbeyaz/multi-server:$GIT_SHA
 docker push cahitbeyaz/multi-worker:$GIT_SHA
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment server=cahitbeyaz/multi-client:$GIT_SHA
-kubectl set image deployments/worker-deployment server=cahitbeyaz/multi-worker:$GIT_SHA
+kubectl set image deployments/client-deployment client=cahitbeyaz/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=cahitbeyaz/multi-worker:$GIT_SHA
 kubectl set image deployments/server-deployment server=cahitbeyaz/multi-server:$GIT_SHA
