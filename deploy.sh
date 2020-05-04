73c5e8cadd110a8eee9c4003fce5d8d08e84ec76
cahit.beyaz <cahit.beyaz@exagate.com>
2020-05-04 11:12:31 +0300

    use GIT_SHA for versioning

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
-docker build -t cahitbeyaz/multi-client -f ./client/Dockerfile .client
-docker build -t cahitbeyaz/multi-server -f ./server/Dockerfile .server
-docker build -t cahitbeyaz/multi-worker -f ./worker/Dockerfile .worker
+docker build -t cahitbeyaz/multi-client -t cahitbeyaz/mult-client:latest -t cahitbeyaz/mult-client:$GIT_SHA -f ./client/Dockerfile .client
+docker build -t cahitbeyaz/multi-server -t cahitbeyaz/mult-server:latest -t cahitbeyaz/mult-server:$GIT_SHA -f ./server/Dockerfile .server
+docker build -t cahitbeyaz/multi-worker -t cahitbeyaz/mult-worker:latest -t cahitbeyaz/mult-worker:$GIT_SHA -f ./worker/Dockerfile .worker
 docker push cahitbeyaz/multi-client
 docker push cahitbeyaz/multi-server
 docker push cahitbeyaz/multi-worker
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cahitbeyaz/multi-server
+kubectl set image deployments/server-deployment server=cahitbeyaz/multi-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=cahitbeyaz/multi-worker:$GIT_SHA
+kubectl set image deployments/server-deployment server=cahitbeyaz/multi-server:$GIT_SHA
