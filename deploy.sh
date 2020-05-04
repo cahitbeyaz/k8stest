c1a1e81abbcab92abd47196865158e3a43b5f6a8
cahit.beyaz <cahit.beyaz@exagate.com>
2020-05-04 11:21:15 +0300

    a quick ammend :)

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ docker push cahitbeyaz/multi-client:$GIT_SHA
 docker push cahitbeyaz/multi-server:$GIT_SHA
 docker push cahitbeyaz/multi-worker:$GIT_SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cahitbeyaz/multi-client:$GIT_SHA
+kubectl set image deployments/client-deployment server=cahitbeyaz/multi-client:$GIT_SHA
 kubectl set image deployments/worker-deployment server=cahitbeyaz/multi-worker:$GIT_SHA
 kubectl set image deployments/server-deployment server=cahitbeyaz/multi-server:$GIT_SHA
